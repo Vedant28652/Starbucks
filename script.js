a1449e70503b8adfbb40387a6e1dc9a576f8cb6e
Vedant28652 <127134871+Vedant28652@users.noreply.github.com>
2023-04-14 14:22:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+alert("j")
+
+
+function imgslide(anything){
+    document.querySelector('.star').src = anything;
+
+}
+
+
+
+function changecolor(color){
+    let temp = document.querySelector('.colors') ;
+
+    temp.style.background = color;
+
+
+
+}
